76902ff4aeecdd05469301fcb1a059d51c2c14e4
Dominic R <110837479+jD2R@users.noreply.github.com>
2024-03-25 17:21:58 -0400

    Fixed a canvas focus bug

--- canvas.js
+++ canvas.js
@@ -10,7 +10,7 @@ export default class Canvas {
         let modules = getModules(imports);
         Object.assign(this, ...modules);
 
-        this.#canvasSetup();
+        this.canvasSetup();
 
         this.width = canvasElement.width;
         this.height = canvasElement.height;
@@ -32,42 +32,58 @@ export default class Canvas {
 
     }
 
-    #canvasSetup () {
+    canvasSetup () {
+        // Removes the outline when the canvas is selected
         this.canvasElement.style.outline = "none";
+        // Adds the tab index so the canvas can be focused
         this.canvasElement.tabIndex = 1;
+        // Focuses the canvas by default for key interaction
+        this.canvasElement.focus();
 
-        this.canvasElement.addEventListener('mousemove', this.#handleMouseOver.bind(this));
+        // ✅
+        this.canvasElement.addEventListener('mousemove', (event) => {
+            let rect = this.canvasElement.getBoundingClientRect();
 
+            this.pmouseX = this.mouseX;
+            this.pmouseY = this.mouseY;
+    
+            this.mouseX = event.clientX - rect.left;
+            this.mouseY = event.clientY - rect.top;
+        });
+
+        // ✅
+        this.canvasElement.addEventListener('click', (event) => {
+            this.canvasElement.focus();
+        });
+
+        // ✅
         this.canvasElement.addEventListener('mousedown', (e) => {
             e.preventDefault();
             this.mouse[this.mouseButtons[e.button]] = true;
         });
+        // ✅
         this.canvasElement.addEventListener('mouseup', (e) => {
             e.preventDefault();
             delete this.mouse[this.mouseButtons[e.button]];
         });
 
-        this.canvasElement.addEventListener('keydown', (e) => this.keys[String(e.key)] = true);
+        // ✅
+        this.canvasElement.addEventListener('keydown', (e) => {
+            e.preventDefault();
+            this.keys[String(e.key)] = true;
+        });
+        // ✅
         this.canvasElement.addEventListener('keyup', (e) => delete this.keys[String(e.key)]);
 
+        // ✅
         this.canvasElement.addEventListener('blur', () => {
             this.keys = {};
             this.mouse = {};
         });
 
+        // ✅
         this.canvasElement.addEventListener('contextmenu', (e) => {
             e.preventDefault();
         });
     }
-
-    #handleMouseOver ( event ) {
-        let rect = this.canvasElement.getBoundingClientRect();
-
-        this.pmouseX = this.mouseX;
-        this.pmouseY = this.mouseY;
-
-        this.mouseX = event.clientX - rect.left;
-        this.mouseY = event.clientY - rect.top;
-    }
-     
 }
